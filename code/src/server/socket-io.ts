668a4c340da47c59d24c1cf086d7a50df0572881
Nikolas Louret <nikoul.ret@gmail.com>
2023-10-25 14:35:47 -0300

    feat: add endpoint para frontend royalrush

--- code/src/server/socket-io.ts
+++ code/src/server/socket-io.ts
@@ -1,20 +1,38 @@
-import { io } from './http'
-import { CommandFunctionType } from '../types/GameType'
+import { io, endpoint } from './http'
+import {
+	CommandFunctionGameType as CommandObserverType,
+	CommandFunctionViewerType as CommandViewerType,
+} from '../types/GameType'
+import { ObserverEnum } from '../types/ObserverEnum'
 import createGame from '../controller/game-instance'
 
 const game = createGame()
 game.start()
 
-io.on('connection', (socket: any) => {
-	const commandFunction = (command: CommandFunctionType) => {
-		socket.emit(command.type, command)
+io.on('connection', socket => {
+	const commandFunction = (command: CommandObserverType) => {
+		socket.emit(command.type, command.state)
 	}
 
-	game.subscribe({ id: socket.id, function: commandFunction })
+	game.subscribe({ id: socket.id, function: commandFunction }, ObserverEnum.LOCAL)
 
-	socket.emit('setup', game.state)
+	socket.emit('setup', game.setupObserver())
 
 	socket.on('disconnect', () => {
-		game.unsubscribe(socket.id)
+		game.unsubscribe(socket.id, ObserverEnum.LOCAL)
+	})
+})
+
+endpoint.on('connection', socket => {
+	const commandFunction = (command: CommandViewerType) => {
+		socket.emit(command.type, command.status)
+	}
+
+	game.subscribe({ id: socket.id, function: commandFunction }, ObserverEnum.ROYAL_RUSH)
+
+	socket.emit('setup', game.setupViewer())
+
+	socket.on('disconnect', () => {
+		game.unsubscribe(socket.id, ObserverEnum.ROYAL_RUSH)
 	})
 })
