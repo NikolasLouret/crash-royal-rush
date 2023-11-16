0bcedee7d522359c6efb1d920f33f85c9f703bba
Nikolas Louret <nikoul.ret@gmail.com>
2023-11-15 21:59:20 -0300

    refactor: add rota '/api' no websocket

--- code/src/server/http.ts
+++ code/src/server/http.ts
@@ -9,13 +9,14 @@ const app = express()
 app.use(express.static(path.join(__dirname, '../../', 'public')))
 app.use(cors())
 
-app.use('/game/', router)
+app.use('/game/api', router)
 
 const server = http.createServer(app)
 
 const io = new Server(server)
 
 const endpoint = new Server(server, {
+	path: '/api',
 	cors: {
 		origin: true,
 		credentials: true,
